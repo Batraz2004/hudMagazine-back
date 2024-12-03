cde46aa4160e2f665c02a76155ddf590c5eba968
Batraz2004 <batraz.tolasovv@gmail.com>
2024-12-03 16:44:06 +0300

    rename resource file end excessdelete

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -12,8 +12,17 @@ class CategoryResource extends JsonResource
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            "title" => $this->title,
+            "description" => $this->description,
+            "parent_id" => $this->parent_id,
+            "image_path" => $this->image_path,
+            "slug" => $this->slug,
+            "sort" => $this->sort,
+            "products" => $this->products
+        ];
     }
 }
